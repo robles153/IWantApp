cd00325e4cc16fe06648447085004ef0c233cc20
robles153 <marcoskim153@gmail.com>
2023-03-13 17:33:15 -0300

    Adicionando propriedades

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,9 +3,10 @@
 public class Product : Entity
 {    
     public string Name { get; set; }
-    public int CategoryId { get; set; }
+    public Guid CategoryId { get; set; }
     public Category Category { get; set; }
     public string Description { get; set; }
     public bool HasStock { get; set; }
+    public bool Active { get; set; } = true;
 
 }
