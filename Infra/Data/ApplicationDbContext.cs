471d1969db9b8d3aca02c1f62d03566a1074cfcc
robles153 <marcoskim153@gmail.com>
2023-03-13 15:35:15 -0300

    Implementando classe de contexto

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .Property(p => p.Name).IsRequired();
+        builder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(255);
+
+        builder.Entity<Category>()
+            .Property(c => c.Name).IsRequired();
+    }
+
+
+    //Configurando tamanho maximo das propriedades do tipo string
+    protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+    {
+        configuration.Properties<string>()
+            .HaveMaxLength(100);
+    }
+}
